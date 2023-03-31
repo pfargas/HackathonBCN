82fc71119c82293faed140365ef42e5b30845d2d
Marc-Farreras <markusfarreras1999@gmail.com>
2023-03-31 18:36:05 +0200

    prova1

---
+++ prova_data2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 09:49:17 2023
+
+@author: marku
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import data
+datos1=[]
+datos2=[]
+datos3=[]
+file=open('Tamb_totes.txt','r')
+contenido=file.readlines()
+for i in range (3,len(contenido),1): 
+   dato1=contenido[i].find('\t')
+   dato2=contenido[i].find('\t',dato1+1)
+   dato3=contenido[i].find('\t',dato2+1)
+
+   
+   
